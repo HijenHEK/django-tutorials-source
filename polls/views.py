859ccaf2e1a88e673a15b7a53af252f14b88fe63
HijenHek <k.hijen@gmail.com>
2021-03-27 10:45:50 +0100

    making templates

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
+
+from django.http import Http404
 # Create your views here.
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
-def details(request , question_id):
-    return HttpResponse('details for question %s' % question_id)
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list' : latest_question_list
+    }
+    
+    return HttpResponse(template.render(context, request))
+    
+    #or use render and remove loader and HttpResponse
+    # return render(request , 'polls/index.html' , context)
+def details(request, question_id):
+
+    question = get_object_or_404(Question, pk=question_id)
+    # or use a try block
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404('Question %s does not exist' % question_id)
+    return render(request, 'polls/details.html', {'question': question})
+    
 def results(request, question_id):
     response = "result for question %s" 
     return HttpResponse(response % question_id)
